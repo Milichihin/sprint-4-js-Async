b9b55df4e2f1a74af56b9bc6d98d7f7d04286426
Milichihin <32966900+Milichihin@users.noreply.github.com>
2021-06-21 12:47:48 +0300

    1st Task complete

--- src/index.js
+++ src/index.js
@@ -12,3 +12,41 @@ Practical tasks. Promise, Async/Await & Generators.
 // Practical tasks. Promise, Async/Await & Generators
 
 console.log("=======================    Question 1   =======================");
+
+// Implement the getPromise(delay, message) function, which takes an integer number delay
+// (between 0 and 2000) and string message and returns a Promise that waits
+// for specified amount of time (using delay argument) and resolves with the message.
+
+function getPromise(delay, message) {
+  let promise = new Promise(function (resolve, reject) {
+    return setTimeout(() => resolve(message), delay);
+  });
+  return promise;
+}
+
+// alternative solution with "Number.isInteger" condition
+
+// function getPromise(delay, message) {
+//   let promise = new Promise(function (resolve, reject) {
+//     return setTimeout(() => {
+//       return Number.isInteger(delay) && delay >= 0 && delay <= 2000
+//         ? (resolve(message), delay)
+//         : (reject(new Error("Whoops!")), delay);
+//       // return Number.isInteger(delay) && delay >= 0 && delay <= 2000
+//       //   ? (resolve(message), delay)
+//       //   : (reject(console.log("Error: Whoops!")), delay);
+//     });
+//   });
+//   return promise;
+// }
+
+getPromise(200, "'hello' in 1st Task").then(function (data) {
+  console.log(data);
+});
+
+const end = Date.now() + 1000;
+while (Date.now() < end) {
+  const muchCompute = 1 + 2 + 3;
+}
+
+console.log("=======================    Question 2   =======================");
